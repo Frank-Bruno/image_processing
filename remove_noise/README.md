ae21d1fc603461af23097cadd600e9adc467b9d4
Frank-Bruno <f.bruno227@gmail.com>
2023-01-15 18:33:31 -0300

    atualizando codigo

---
+++ remove_noise/README.md
@@ -0,0 +1,7 @@
+# Criação das imagens com ruidos
+Para cada imagem processada foi gerado uma nova para cada valor de ruído e densidade diferente.
+* Ruído salt & pepper com densidade de 0.06 e 0.005
+* Ruído Gaussiano com densidade de 0.001 e 0.03.
+
+O padrão de nome das imagens criadas foi:
+nome
--- remove_noise/Random_noise.py
+++ remove_noise/Random_noise.py
@@ -3,35 +3,25 @@ from skimage.util import random_noise
 from skimage.util import img_as_ubyte
 from pathlib import Path
 
-#Class for make noising imagens
-class Random_noise:
-    def __init__(self, path=Path):
-        self.path = path
-        self.im_original = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
-
-    def make_img_noise(self):
+def creating_noise(path=Path):
+    path = path
+    im_original = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
         
-        sp_noise_01 = random_noise(self.im_original, mode='s&p', seed=None, clip=True, salt_vs_pepper=0.06)
-        sp_noise_02 = random_noise(self.im_original, mode='s&p', seed=None, clip=True, salt_vs_pepper=0.005)
-        gauss_noise_01 = random_noise(self.im_original, mode='gaussian', seed=None, clip=True, var=0.001)
-        gauss_noise_02 = random_noise(self.im_original, mode='gaussian', seed=None, clip=True, var=0.03)
-
-        #sp_noise_01 = np.array(255*sp_noise_01, dtype='uint8')
-        # sp_noise_02 = np.array(255*sp_noise_02, dtype='uint8')
-        # gauss_noise_01 = np.array(255*gauss_noise_01, dtype='uint8')
-        # gauss_noise_02 = np.array(255*gauss_noise_02, dtype='uint8')
+    sp_noise_01 = random_noise(im_original, mode='s&p', seed=None, clip=True, salt_vs_pepper=0.06)
+    sp_noise_02 = random_noise(im_original, mode='s&p', seed=None, clip=True, salt_vs_pepper=0.005)
+    gauss_noise_01 = random_noise(im_original, mode='gaussian', seed=None, clip=True, var=0.001)
+    gauss_noise_02 = random_noise(im_original, mode='gaussian', seed=None, clip=True, var=0.03)
         
-        cv2.imwrite(str(Path(self.path.parent,"imgs_original",self.path.stem+"_img_original.png")), self.im_original)
-        cv2.imwrite(str(Path(self.path.parent,"imgs_noise",self.path.stem+"_01.png")), img_as_ubyte(sp_noise_01))
-        cv2.imwrite(str(Path(self.path.parent,"imgs_noise",self.path.stem+"_02.png")), img_as_ubyte(sp_noise_02))
-        cv2.imwrite(str(Path(self.path.parent,"imgs_noise",self.path.stem+"_03.png")), img_as_ubyte(gauss_noise_01))
-        cv2.imwrite(str(Path(self.path.parent,"imgs_noise",self.path.stem+"_04.png")), img_as_ubyte(gauss_noise_02))
+    cv2.imwrite(str(Path(path.parent,"imgs_original",path.stem+"_img_original.png")), im_original)
+    cv2.imwrite(str(Path(path.parent,"imgs_noise",path.stem+"_01.png")), img_as_ubyte(sp_noise_01))
+    cv2.imwrite(str(Path(path.parent,"imgs_noise",path.stem+"_02.png")), img_as_ubyte(sp_noise_02))
+    cv2.imwrite(str(Path(path.parent,"imgs_noise",path.stem+"_03.png")), img_as_ubyte(gauss_noise_01))
+    cv2.imwrite(str(Path(path.parent,"imgs_noise",path.stem+"_04.png")), img_as_ubyte(gauss_noise_02))
 
 
 path = Path(r'/home/frank/Imagens')
 lista = list(path.iterdir())
 for p in path.iterdir():
     if p.is_file():
-        l = Random_noise(p)
-        l.make_img_noise()
-        
+        creating_noise(p)
+                
